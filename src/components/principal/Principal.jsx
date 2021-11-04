812c5957d5bd6262bfe6dc3a9c0700e3476bbb0c
Alejandro Motta <alejandromotta808@gmail.com>
2021-11-03 23:58:46 -0500

    portfolio

--- src/components/principal/Principal.jsx
+++ src/components/principal/Principal.jsx
@@ -10,12 +10,12 @@ export default function principal(){
             </h1>
             <h1 className="accessibility-animation">
                 <span className="end-letters">Full Stack Js Dev </span><span className="middle-letters"> , Do you have a
-                    problem? </span><span class="alternate-middle-letters">, I have a solution.</span>
+                    problem? </span><span className="alternate-middle-letters">, I have a solution.</span>
             </h1>
             <div>
-            <a href="https://github.com/alejandromtta" target="_blank" ><img src={github} alt="github" className="logo"/></a>
-            <a href="https://www.linkedin.com/in/alejandro-mtta/" target="_blank"  ><img src={linkedin} alt="linkedin" className="logo"/></a>
-            <a href="https://github.com/alejandromtta" target="_blank" ><img src={twitter} alt="twitter" className="logo"/></a>
+            <a href="https://github.com/alejandromtta" target="_blank"  rel="noreferrer" ><img src={github} alt="github" className="logo"/></a>
+            <a href="https://www.linkedin.com/in/alejandro-mtta/" target="_blank"   rel="noreferrer" ><img src={linkedin} alt="linkedin" className="logo"/></a>
+            <a href="https://github.com/alejandromtta" target="_blank"  rel="noreferrer"  ><img src={twitter} alt="twitter" className="logo"/></a>
             </div>
                 
         </div>
